03461dd128df60e6f070942a20f5cb6f6c28e010
Aleksis Brezas <abresas@gmail.com>
2014-06-27 23:45:42 +0300

    add cabal files, license and readme

--- AI.hs
+++ AI.hs
@@ -1,3 +1,26 @@
+{-|
+    Module:         AI
+    Description:    Create and train neural networks
+    Copyright:      (c) Aleksis Brezas, 2014 
+    License:        MIT
+
+    Stability:      experimental
+    Portability:    POSIX
+-}
+module AI ( 
+    -- * Types
+    Neuron, NeuralLayer, NeuralNetwork, 
+    -- * Neural network
+    createNN3, runNN, runNN1, runNN2,
+    getInputSize, getOutputSize,
+    -- ** Layer
+    createSigmoidLayer, createLinearLayer, initLayerWeights,
+    -- ** Neuron
+    createSigmoidNeuron, createLinearNeuron, initNeuronWeights, activate,
+    -- * Training
+    backProp, trainNN, validateNN, testTrainNN
+    ) where
+
 import System.Random
 import System.IO
 import Data.List
@@ -5,40 +28,50 @@ import Control.Monad
 import Control.Concurrent
 import Debug.Trace
 
+-------------------------- GENERIC -----------------------------
+
+-- |Given a function f and an initial value x,
+-- call f repetitively n times, producing the result f(f(..f(x)..)).
+loop :: Int -> (a -> a) -> a -> a
+loop n = ((!! n) .) . iterate
+
 --------------------------- MATH --------------------------------
 
--- Calculate average (or mean) of list of reals.
+-- |Calculate average (or mean) of list of reals.
 -- = ( 1 / n ) * sum
 mean :: Floating a => [a] -> a
 mean xs = (sum xs) / genericLength xs
 
--- Normalize vector
+-- |Normalize vector
 norm :: Floating a => [a] -> a
 norm = sqrt . sum . map (** 2)
 
--- Sum the inner product of two lists
+-- |Sum the inner product of two lists
 sumProduct :: Num a => [a] -> [a] -> a
 sumProduct = (sum .) . zipWith (*)
 
 ------------------------ STATISTICS -----------------------------
 
--- Calculate error of prediction vector as percentage
+-- |Calculate error of prediction vector as percentage.
 percentError :: Floating a => [a] -> [a] -> a
 percentError target output = let
     e = zipWith (-) target output
     in 100 * ( (norm e) / (norm target) )
 
--- Calculate mean square error of a list of targets and list of predictions
--- The result has a magnitude of the inputs squared, so for a result of same magnitude as inputs, call rmse
+-- |Calculate mean square error of a list of targets and list of predictions.
+--
+-- The result has a magnitude of the inputs squared, so for a result of same magnitude as inputs, call rmse.
 mse :: Floating a => [a] -> [a] -> a
 mse = (mean .) . zipWith ( ( (** 2) . ) . (-) )
 
--- Calculate root mean square error of a list of targets and list of predictions
--- Targets and predictions must be of type double
+-- |Calculate root mean square error of a list of targets and list of predictions.
+--
+-- Targets and predictions must be of type double.
 rmse :: Floating a => [a] -> [a] -> a
 rmse = (sqrt .) . mse
 
--- Calculate root mean square error of normalized targets and normalized predictions
+-- |Calculate root mean square error of normalized targets and normalized predictions.
+--
 -- This is like RMSE but first normalizes targets and predictions
 normRMSE :: Floating a => [[a]] -> [[a]] -> a
 normRMSE t p = rmse (map norm t) (map norm p)
@@ -47,7 +80,7 @@ normRMSE t p = rmse (map norm t) (map norm p)
 
 type Seed = StdGen
 
--- Generate a list of finite randoms.
+-- |Generate a list of finite randoms.
 finiteRandoms :: (Random a) => Int -> Seed -> ([a], Seed)  
 finiteRandoms 0 gen = ([], gen)  
 finiteRandoms n gen =   
@@ -69,7 +102,7 @@ instance Show Neuron where
 type NeuralLayer = [Neuron]
 type NeuralNetwork = [NeuralLayer]
 
--- Feed-forward activate neuron with given input.
+-- |Feed-forward activate neuron with given input.
 activate :: [Double] -> Neuron -> Double
 activate i n = 
     let g = function n
@@ -77,7 +110,7 @@ activate i n =
         sigma = sum $ zipWith (*) i w
     in g sigma
 
--- Feed-backward activate neuron with given input and weighted delta.
+-- |Feed-backward activate neuron with given input and weighted delta.
 backwardActivate :: Neuron -> [Double] -> Double -> Double
 backwardActivate n i weightedDelta =
     let g' = derivative n
@@ -85,37 +118,43 @@ backwardActivate n i weightedDelta =
         last_in = sum $ zipWith (*) i w
     in (g' last_in) * weightedDelta
 
--- Logistic function :: R -> [0..1]
+-- |Logistic function :: R -> [0..1]
 sigmoid :: Double -> Double
 sigmoid x = (exp x)/(1 + exp x)
 
--- Derivative of logistic function.
+-- |Derivative of logistic function.
 sigmoidDerivative :: Double -> Double
 sigmoidDerivative x = (exp x)/((exp x + 1)**2)
 
--- Create a neuron with sigmoid activation using given weights.
+-- |Create a neuron with sigmoid activation using given weights.
 createSigmoidNeuron :: [Double] -> Neuron
 createSigmoidNeuron w = Neuron w sigmoid sigmoidDerivative
 
--- Create a linear neuron with given weights.
+-- |Create a linear neuron with given weights.
+--
 -- May be useful for output layer.
+--
 -- Curently uses identity function as activation.
 createLinearNeuron :: [Double] -> Neuron
 createLinearNeuron w = Neuron w id (\x -> 1)
 
--- Create a layer of sigmoid neurons.
+-- |Create a layer of sigmoid neurons.
 createSigmoidLayer :: [[Double]] -> NeuralLayer
 createSigmoidLayer = map createSigmoidNeuron
 
--- Create a layer of linear neurons.
+-- |Create a layer of linear neurons.
+--
 -- May be useful as output layer.
 createLinearLayer :: [[Double]] -> NeuralLayer
 createLinearLayer = map createLinearNeuron
 
--- Return a specific number of initial weights
--- The weights are adjusted to be a random number between -0.5 and 0.5
--- These are considered "acceptable" weights, but
+-- |Return a specific number of initial weights.
+--
+-- The weights are adjusted to be a random number between -0.5 and 0.5.
+--
+-- These are considered \"acceptable\" weights, but
 -- many studies have proved that there are much better initial values.
+--
 -- This needs some work.
 initNeuronWeights :: Int -> Seed -> ([Double], Seed)
 initNeuronWeights numWeights seed =
@@ -123,7 +162,7 @@ initNeuronWeights numWeights seed =
         weights = map (\x -> x - 0.5 ) r
     in (weights, seed')
 
--- Use initNeuronWeights to generate initial weights for whole layer.
+-- |Use initNeuronWeights to generate initial weights for whole layer.
 initLayerWeights :: Int -> Int -> Seed -> ([[Double]], Seed)
 initLayerWeights 0 numWeights seed = ([], seed)
 initLayerWeights numNeurons numWeights seed = 
@@ -131,37 +170,54 @@ initLayerWeights numNeurons numWeights seed =
         (rest, seed1) = initLayerWeights (numNeurons-1) numWeights seed0
     in ( (firstWeights:rest), seed1 )
 
--- Create a 3-layer neural network (input, 1 hidden, output layers).
--- One of the most important functions in this code.
+-- |Create a 3-layer neural network (input, hidden, output).
+--
 -- The neural network doesnt store representation of the input layer,
 -- since it doesnt have any weights.
-createNN3 :: Seed -> Int -> Int -> Int -> NeuralNetwork
+createNN3 :: Seed -> Int -> Int -> Int -> (NeuralNetwork,Seed)
 createNN3 gen numInput numHidden numOutput = 
     let (hiddenWeights, gen') = initLayerWeights numHidden numInput gen
-        (outputWeights, _) = initLayerWeights numOutput numHidden gen'
+        (outputWeights, gen'') = initLayerWeights numOutput numHidden gen'
         hiddenLayer = createSigmoidLayer hiddenWeights
         outputLayer = createSigmoidLayer outputWeights
-    in [hiddenLayer, outputLayer]
+    in ([hiddenLayer, outputLayer],gen'')
 
--- Run a multi-layer neural network and return its output.
+-- | Return size of input layer.
+getInputSize :: NeuralNetwork -> Int
+getInputSize = length . weights . head . head
+
+-- | Return size of output layer.
+getOutputSize :: NeuralNetwork -> Int
+getOutputSize = length . last
+
+-- |Run a multi-layer neural network and return its output.
 runNN :: NeuralNetwork -> [Double] -> [Double]
 runNN n i = runNNLayers i n
 
--- Run each layer of this neural network and return the last as output.
+-- |Run a neural network with 1 input and 1 output
+runNN1 :: NeuralNetwork -> Double -> Double
+runNN1 n i = head $ runNNLayers [i] n
+
+-- |Run a neural network with 2 inputs and 1 output
+runNN2 :: NeuralNetwork -> Double -> Double -> Double
+runNN2 n i1 i2 = head $ runNNLayers [i1,i2] n
+
+-- |Run each layer of this neural network and return the last as output.
 runNNLayers :: [Double] -> [NeuralLayer] -> [Double]
 runNNLayers = foldl runNNLayer
 
--- Run layer and return output.
+-- |Run layer and return output.
 runNNLayer :: [Double] -> NeuralLayer -> [Double]
 runNNLayer = zipWith activate . repeat
 
--- Like runNN but returns list of output per layer (output matrix).
+-- |Like runNN but returns list of output per layer (output matrix).
 layersOutput :: [Double] -> [NeuralLayer] -> [[Double]]
 layersOutput input n = foldl (\outputs l -> outputs ++ [(runNNLayer (last outputs) l)]) [input] n
 
 ------------------------ NN LEARNING --------------------------------
 
--- Update weights of a neuron given learning rate, delta and input.
+-- |Update weights of a neuron given learning rate, delta and input.
+--
 -- The formula is Wij = Wij + l * aj * Di.
 updateWeights :: Double -> Neuron -> Double -> [Double] -> Neuron
 updateWeights learning_rate n delta inputs = 
@@ -171,20 +227,27 @@ updateWeights learning_rate n delta inputs =
         updatedWeights = zipWith (\weight input -> weight + learning_rate * input * delta) w inputs
     in Neuron updatedWeights func der
 
--- Update weights of layer given learning rate, list of neurons, list of deltas, list of input to neuron.
+-- |Update weights of layer given learning rate, list of neurons, list of deltas, list of input to neuron.
 updateWeightsLayer :: Double -> NeuralLayer -> [Double] -> [[Double]] -> NeuralLayer
 updateWeightsLayer = zipWith3 . updateWeights
 
--- Update weights on neural network after back propagation.
+-- |Update weights on neural network after back propagation.
+--
 -- Parameters: learning rate, neural network, matrix of deltas (per neuron), matrix of input to neuron
 updateWeightsNN :: Double -> NeuralNetwork -> [[Double]] -> [[[Double]]] -> NeuralNetwork
 updateWeightsNN = zipWith3 . updateWeightsLayer
 
--- Calculates weighted deltas for a layer.
--- Equal to for each neuron j: Σi Wij Dj
--- Wij weights from i to j
--- Dj delta of neuron j
--- Σi sum for each connected neuron i
+-- |Calculates weighted deltas for a layer.
+--
+-- Equal to:
+--
+--      for each neuron j: Σi Wij Dj
+--
+-- Where:
+--
+--      * Wij weights from i to j
+--      * Dj delta of neuron j
+--      * Σi sum for each connected neuron i
 weightSigmaLayer :: [Double] -> NeuralLayer -> [Double]
 weightSigmaLayer delta layer = let
     -- using tranpose we get weights grouped by source node
@@ -192,15 +255,15 @@ weightSigmaLayer delta layer = let
     w = transpose $ map (\n -> (weights n)) layer
     in zipWith sumProduct w (repeat delta)
 
--- Calculate delta for each neuron in layer.
+-- |Calculate delta for each neuron in layer.
 layerDelta :: NeuralLayer -> [[Double]] -> [Double] -> [Double]
 layerDelta = zipWith3 backwardActivate
 
--- Transform matrix of outputs to matrix of inputs.
+-- |Transform matrix of outputs to matrix of inputs.
 outputsToInputs :: [[Double]] -> [[[Double]]]
 outputsToInputs = init . (map repeat)
 
--- Calculate delta for each neuron in each layer of neural network.
+-- |Calculate delta for each neuron in each layer of neural network.
 layersDelta :: [[[Double]]] -> [Double] -> [[Double]] -> [NeuralLayer] -> [[Double]]
 layersDelta inputMatrix prevError acc [] = acc
 layersDelta inputMatrix prevError acc layers = let
@@ -210,17 +273,20 @@ layersDelta inputMatrix prevError acc layers = let
     layerError = weightSigmaLayer delta l
     in layersDelta inputMatrix' layerError (delta:acc) layers'
 
--- Backpropagate multi-layer network using a single training sample (input,target) 
+-- |Backpropagate multi-layer network using a single training sample (input,target) 
+--
 -- learning_rate is the speed of learning, typically in the range [0..1], usually 0.1.
+--
 -- Large learning_rate allows for faster learning, but smaller values will typically result in better learning.
 --
 -- A training function will call this function multiple times
 -- to get a network with improved weights every time.
 --
 -- Example teaching sine function:
---  n = createNN3 seed 1 10 1
---  n' = backProp n [0] [0] 0.1
---  n'' = backProp n' [pi/2] [1] 0.1
+--
+-- > let (n,_) = createNN3 seed 1 10 1
+-- > let n' = backProp n [0] [0] 0.1
+-- > let n'' = backProp n' [pi/2] [1] 0.1
 --
 -- Different learning rates may be used during the training of the same network,
 -- for example by passing large values at the beginning and smaller ones later.
@@ -241,59 +307,33 @@ backProp n input target learning_rate = let
     -- Use deltas to update weights
     in updateWeightsNN learning_rate n deltas inputs 
 
---------------------- NEURAL NETWORK APPLICATIONS ---------------------
+-- |Train a neural network to learn a function ([Double] -> [Double])
+-- The function will be called with random input in [0..1]
+-- and is expected to have output in [0..1]
+trainNN :: NeuralNetwork -> ([Double] -> [Double]) -> Double -> Int -> Seed -> (NeuralNetwork,Seed)
+trainNN nn _ _ 0 gen = (nn, gen)
+trainNN nn func learningRate times gen = let
+    numInput = getInputSize nn
+    (input,gen') = finiteRandoms numInput gen :: ([Double],Seed)
+    target = func input
+    nn' = backProp nn input target learningRate
+    in trainNN nn' func learningRate (times-1) gen'
 
--- Generate a neural network trained to calculate the cosine function
-trainCos :: IO NeuralNetwork
-trainCos = do
-    gen <- getStdGen
-    let n = createNN3 gen 1 10 1
-    trainCosLoop n 10000 0 1
+-- |Validate how well the neural network has learned the given function.
+validateNN :: NeuralNetwork -> ([Double] -> [Double]) -> Int -> Seed -> (Double,Seed)
+validateNN nn func times gen = let
+    (targets,outputs,gen') = loop times ( \(t,o,g) -> let
+        numInput = getInputSize nn
+        (input,g') = finiteRandoms numInput g :: ([Double],Seed)
+        target = func input
+        output = runNN nn input
+        in (target:t,output:o,g') ) ([],[],gen)
+    in (normRMSE targets outputs, gen')
 
-trainCosLoop :: NeuralNetwork -> Int -> Int -> Double -> IO NeuralNetwork
-trainCosLoop n times timesTrained lastRmse = do
-    if ( times == timesTrained )
-        then do return n
-        else do
-            gen <- newStdGen
-            r <- randomIO :: IO Double
-            let input = r * pi / 2
-            let target = [cos input]
-            let n' = backProp n [input] target 1.5
-            let e = validateCosLoop gen n 1000 [] []
-            putStrLn $ "Root mean square error: " ++ (show e) ++ " (" ++ (show timesTrained) ++ ")"
-            trainCosLoop n' times (timesTrained + 1) e
-
-validateCosLoop :: Seed -> NeuralNetwork -> Int -> [[Double]] -> [[Double]] -> Double
-validateCosLoop gen n 0 outputs targets = normRMSE targets outputs
-validateCosLoop gen n times outputs targets = let 
-    (r,gen') = random gen
-    input = r * pi / 2
-    target = [cos input]
-    output = runNN n [input]
-    in validateCosLoop gen' n (times-1) (output:outputs) (target:targets)
-
--- Start a game of guessing the function the user has in mind.
--- The function must have 2 inputs and 1 output.
-guessGame :: IO ()
-guessGame = do 
-    hSetBuffering stdin LineBuffering -- fix backspace/delete in ghci
-    gen <- getStdGen
-    let n = createNN3 gen 2 10 1
-    guessGameLoop n
-
-guessGameLoop :: NeuralNetwork -> IO ()
-guessGameLoop n = do
-    putStr "Give me two numbers: "
-    inputString <- getLine
-    when ( not $ null inputString ) $ do
-        let numberStrings = words inputString
-        let input = map ((/ 100) . read) numberStrings
-        let output = runNN n input
-        putStrLn $ "My guess: " ++ (show (round (100 * head output)))
-        putStr "What's the correct result? "
-        resultString <- getLine
-        when ( not $ null resultString ) $ do 
-            let result = (/ 100) $ read resultString
-            let n' = backProp n input [result] 5
-            guessGameLoop n'
+-- |Train neural network and return the validation results directly.
+--
+-- Useful for testing the library.
+testTrainNN :: NeuralNetwork -> ([Double] -> [Double]) -> Double -> Int -> Seed -> (Double,Seed)
+testTrainNN nn func learningRate times gen = let
+    (nn',gen') = trainNN nn func learningRate times gen
+    in validateNN nn' func 1000 gen'
---
+++ README.md
@@ -0,0 +1,78 @@
+Intro
+=====
+
+A small library for neural networks written in haskell.
+
+I made it just for fun and it's completely experimental.
+
+It probably needs a better name :)
+
+Installation
+============
+
+Download or clone the project and run:
+
+    $ cabal configure
+    $ cabal install
+
+You will need to have cabal-install package installed in your system.
+
+Usage
+=====
+
+Create a 3-layer neural network n, having 1 input, 10 hidden, and 1 output nodes, with initial seed gen:
+
+    >>> gen <- newStdGen
+    >>> let (n,gen') = createNN3 gen 1 10 1
+
+*createNN3* like most functions in these examples, because they need randomnes, require a seed parameter and return a pair with the result and the next state of the seed.
+
+Run the neural network with *runNN*
+
+    >>> runNN n [0]
+    [0.4850840962158099]
+
+Train the neural network to learn the cosine in [0..1]:
+
+    >>> let (n',gen'') = trainNN n (\xs -> [ cos $ head xs ] ) 0.9 1000 gen'
+
+The training function must have type [ Double ] -> [ Double ],
+so in the 2nd parameter we transform cos into accepting and returning a list.
+
+0.9 is the learning rate, and 1000 is the times to repeat the training.
+
+Now we can run the neural network again for varius inputs to see the output:
+
+    >>> runNN n' [0]
+    [0.8840487237322846]
+    >>> runNN n' [0.5]
+    [0.8376510812538144]
+    >>> runNN n' [1]
+    [0.784193327107528]
+
+We can train the neural network more times to gain better results:
+
+    >>> let (n',gen'') = trainNN n (\xs -> [ cos $ head xs ] ) 0.9 5000 gen'
+    >>> runNN n' [1]
+    [0.5629223322703413]
+
+which is much closer to the actual value of cos 1 = 0.54.
+
+*runNN1* takes a neural network that has 1 input and 1 output, and a double to be used as input.
+
+    >>> runNN1 n' 0
+    [0.9846716920261342]
+
+*runNN1* can also be seen as a function that transforms neural network into a function accepting a double:
+    >>> let mycos = runNN1 n'
+    >>> :t mycos
+    mycos :: Double -> Double
+    >>> mycos 0
+    0.9846716920261342
+
+We can validate how well our neural network has learned the given function by calling *validateNN*:
+    >>> let (e,_) = validateNN n' (\xs -> [ cos $ head xs ] ) 1000 gen''
+    >>> e
+    1.2427629031998429e-2
+    
+*validateNN* returns the Root Mean Square Error of the results of the neural network compared to the results of the target function. Each result and target output is normalized before calculating, so the error is always returned as a Double (even if the neural network has multiple inputs and outputs).
